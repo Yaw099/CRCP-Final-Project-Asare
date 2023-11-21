ae614bfde97a7fca624375f8b3fe7fafb9072d43
Yaw099 <ledavido099@gmail.com>
2023-11-20 19:52:12 -0600

    updated MarkovModel

--- src/main/java/com/example/MarkovModel.java
+++ src/main/java/com/example/MarkovModel.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class MarkovModel {
     private int currentOrder;
     private List<WeatherDay> weatherData;
+    private Map<String, Map<String, Double>> transitionProbabilities;
 
     /**
      * Initializes the Markov Model with weather data.
@@ -34,5 +35,28 @@ public class MarkovModel {
     public WeatherDay predictTomorrow(String currentDate) {
         // Placeholder
         return new WeatherDay("predictedDate", 0, 0, 0, 0.f, 0, 0, "predictedDeparture", 0.f, "predictedDepth");
+    }
+
+    /**
+     * Initializes the transition probabilities of the Markov Model based on historical weather data.
+     */
+    public void initializeTransitionProbabilities() {
+        transitionProbabilities = new HashMap<>();
+
+        // Example: Process weather data to calculate initial transition probabilities
+        for (int i = 0; i < weatherData.size() - 1; i++) {
+            String currentState =  weatherData.get(i).toString(); // Simplified representation of weather state
+            String nextState = weatherData.get(i + 1).toString();
+
+            transitionProbabilities.putIfAbsent(currentState, new HashMap<>());
+            Map<String, Double> stateTransitions = transitionProbabilities.get(currentState);
+            stateTransitions.put(nextState, stateTransitions.getOrDefault(nextState, 0.0) + 1);
+        }
+
+        // Normalize probabilities
+        for (Map<String, Double> stateTransitions : transitionProbabilities.values()) {
+            double total = stateTransitions.values().stream().mapToDouble(Double::doubleValue).sum();
+            stateTransitions.replaceAll((key, value) -> value / total);
+        }
     }
 }
